6c5e0821af4e9ad100567b274fa5627683feac58
Sébastien Santoro <dereckson@espace-win.org>
2018-04-17 01:10:08 +0000

    Switch uuid to Keruald\OmniTools\Identifiers\UUID
    Summary:
    This is a follow-up for change 8eccc724dbe9.
    
    Ref T1395
    
    Test Plan: Run tests
    
    Reviewers: dereckson
    
    Reviewed By: dereckson
    
    Maniphest Tasks: T1395
    
    Differential Revision: https://devcentral.nasqueron.org/D1612

--- core.php
+++ core.php
@@ -2,6 +2,7 @@
 
 namespace Keruald;
 
+use Keruald\OmniTools\Identifiers\UUID;
 use Keruald\OmniTools\Network\IP;
 use Keruald\OmniTools\Strings\Multibyte\StringUtilities;
 
@@ -73,33 +74,10 @@ function is_ipv6 ($string) {
  * Generates a RFC 4211 compliant v4 UUID (random-based)
  *
  * @return string the UUID
+ * @deprecated Use Keruald\OmniTools\Identifiers\UUID::UUIDv4
  */
 function uuid () {
-    //Code by Andrew Moore
-    //See http://php.net/manual/en/function.uniqid.php#94959
-    //    https://www.ietf.org/rfc/rfc4122.txt
-
-    return sprintf(
-        '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-
-        // 32 bits for "time_low"
-        mt_rand(0, 0xffff), mt_rand(0, 0xffff),
-
-        // 16 bits for "time_mid"
-        mt_rand(0, 0xffff),
-
-        // 16 bits for "time_hi_and_version",
-        // four most significant bits holds version number 4
-        mt_rand(0, 0x0fff) | 0x4000,
-
-        // 16 bits, 8 bits for "clk_seq_hi_res",
-        // 8 bits for "clk_seq_low",
-        // two most significant bits holds zero and one for variant DCE1.1
-        mt_rand(0, 0x3fff) | 0x8000,
-
-        // 48 bits for "node"
-        mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
-    );
+    return UUID::UUIDv4();
 }
 
 ///
